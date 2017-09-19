f4f98f27683278f5727b49922a153c6e97ac682e
yangcx <yangcx@thinkingbar.cn>
2017-09-19 14:15:01 +0800

    xml文件读取

--- src/cn/cxy/designpattern/factory/spring/containerMock/ClassPathXmlApplicationContext.java
+++ src/cn/cxy/designpattern/factory/spring/containerMock/ClassPathXmlApplicationContext.java
@@ -1,13 +1,14 @@
 package cn.cxy.designpattern.factory.spring.containerMock;
 
-import java.util.HashMap;
+import org.dom4j.Attribute;
+import org.dom4j.Element;
+import org.dom4j.io.DOMReader;
+import org.w3c.dom.Document;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import java.util.HashMap;
+import java.util.Iterator;
 
 /**
  * @ClassName: ClassPathXmlApplicationContext
@@ -16,38 +17,36 @@ import org.w3c.dom.NodeList;
  * @date: 2017年9月18日 下午10:56:29
  */
 public class ClassPathXmlApplicationContext {
-	/**
-	 * 用于模拟容器池
-	 */
-	private HashMap<String, Object> map = new HashMap<>();
-	
-	/**
-	 * 模拟从容器中获取bean
-	 */
-	public Object getBean(String name) {
-		return map.get(name);
-	}
+    /**
+     * 用于模拟容器池
+     */
+    private HashMap<String, Object> map = new HashMap<>();
 
-	/**
-	 * 模拟加载实体类到工厂容器中
-	 */
-	public ClassPathXmlApplicationContext(String location) throws Exception {
-		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder builder = factory.newDocumentBuilder();
-		Document document = builder.parse(this.getClass().getClassLoader().getResourceAsStream(location));
-		NodeList nodeList = document.getElementsByTagName("bean");
-		if (null != nodeList && nodeList.getLength() > 0) {
-			for (int i = 0; i < nodeList.getLength(); i++) {
-				Node item = nodeList.item(i);
-				NodeList children = item.getChildNodes();
-				if (null != children && children.getLength() > 0) {
-					for (int j = 0; j < children.getLength(); j++) {
-						Node child = children.item(i);
-						System.out.println(child.getAttributes());
-					}
-				}
-			}
-		}
-	}
+    /**
+     * 模拟从容器中获取bean
+     */
+    public Object getBean(String name) {
+        return map.get(name);
+    }
+
+    /**
+     * 模拟加载实体类到工厂容器中  --  使用dom4j读取配置文件
+     */
+    public ClassPathXmlApplicationContext(String location) throws Exception {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(this.getClass().getClassLoader().getResourceAsStream(location));
+        DOMReader domReader = new DOMReader();
+        org.dom4j.Document dom4jDoc = domReader.read(document);
+        Element rootElement = dom4jDoc.getRootElement();
+        Iterator<Element> iterator = rootElement.elementIterator();
+        while (iterator.hasNext()) {
+            Element next = iterator.next();
+            Attribute id = next.attribute("id");
+            Attribute aClass = next.attribute("class");
+            Object o = Class.forName(aClass.getValue()).newInstance();
+            map.put(id.getValue(),o);
+        }
+    }
 
 }
