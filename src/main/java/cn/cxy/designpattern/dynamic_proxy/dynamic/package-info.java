65eab8dc272019d0c6a45660e1084a9c9662261c
yangcxx <charlesyoung0811@gmail.com>
2021-08-27 15:13:20 +0800

    Update package-info.java

--- src/main/java/cn/cxy/designpattern/dynamic_proxy/dynamic/package-info.java
+++ src/main/java/cn/cxy/designpattern/dynamic_proxy/dynamic/package-info.java
@@ -7,3 +7,14 @@
  * @since: Thinkingbar Web Project 1.0
  */
 package cn.cxy.designpattern.dynamic_proxy.dynamic;
+
+/**
+ * 每一种需求都要增加一个代理,TankLogProxy TankTimeProxy...   ===>  todo 动态生成这些类
+ *
+ *
+ * 现在动态代理，动态生成的代理类是写死了的，是用字符串写死在类里面的，而且，只能动态生成实现了 Moveable 接口的代理，如果要实现任意接口的代理应该怎么办？
+ * 那就不将动态生成代理类的字符串写死，动态拼接生成代理类
+ *
+ *
+ * fixme 生成的这些类都是固定的,只是实现当前接口,无法处理实现其他接口的情况  ==>  动态拼接生成代理类
+ */
