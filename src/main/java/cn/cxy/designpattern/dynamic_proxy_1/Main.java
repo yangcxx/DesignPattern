904b7610ff21bb820c20513e819cb4b23e6a31db
yangcxx <charlesyoung0811@gmail.com>
2021-08-29 13:31:45 +0800

    Update Main.java

--- src/main/java/cn/cxy/designpattern/dynamic_proxy_1/Main.java
+++ src/main/java/cn/cxy/designpattern/dynamic_proxy_1/Main.java
@@ -1,1 +1,20 @@
+package cn.cxy.designpattern.dynamic_proxy_1;
+
+/**
+ * @author yWX929313
+ * @date 2021/8/27 14:46
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // Tank tank = new Tank();
+        // TankTimeProxyOriginal timeProxy = new TankTimeProxyOriginal(tank);
+        // timeProxy.move();
+
+
+        Moveable moveable = (Moveable) Proxy.newProxyInstance();
+        moveable.move();
+    }
+
+}
 
